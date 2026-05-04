f3b85063ad061df8cc74910b02de7fe9cb5405e9
unknown <phanngocduong2k6@gmail.com>
2026-05-04 08:33:22 +0700

    b2

---
+++ MDK-ARM/RTE/_b2_chongrung_lt/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'b2(chongrung)lt' 
+ * Target:  'b2(chongrung)lt' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/b2(chongrung)lt/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+b2(chongrung)lt\startup_stm32f103xb.o: startup_stm32f103xb.s
